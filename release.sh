d22599c401b8036a3df22427eb4b6b7d26826132
Felix Leipold <felix.leipold@gmail.com>
2014-12-22 22:42:27 +0100

    Create branch for release

--- release.sh
+++ release.sh
@@ -19,17 +19,25 @@ if [ "${dirty}" ]; then
     exit 1
 fi >&2
 
+git pull --rebase
+
 find . -name target -prune -exec rm -r {} \;
 
 sbt "set version in ThisBuild := \"${version}\"" +test +publishSigned
 
 tag="v${version}"
 
+git branch --force "release-${tag}"
+git checkout "release-${tag}"
 
 sed -i "" -E 's/(.*"org.programmiersportgruppe.akre" %% "[^"]*" % ")[^"]*(.*)/\1'"${version}"'\2/' README.md
-
 git commit -a -m "Releasing ${version}." || echo "Nothing to commit"
 
 git tag -f "${tag}"
 
-git push origin "${tag},master"
+git push origin "${tag}" "${tag}:master"
+
+git checkout master
+git pull --rebase
+
+git branch -d "release-${tag}"
